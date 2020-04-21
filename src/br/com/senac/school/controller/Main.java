f8bbcf886493629a9a228f434c6c7c2dee4eb856
Gabryelle SS <gabryelle-gaby@hotmail.com>
2020-04-21 20:49:37 -0300

    feat: inserting MVC structure controller
    Resolves: PROJ-48

---
+++ src/br/com/senac/school/controller/Main.java
@@ -0,0 +1,29 @@
+package br.com.senac.school.controller;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/com/senac/school/view/Main.fxml"));
+			AnchorPane pane = loader.load();
+			Scene mainScene = new Scene(pane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("School Map");
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
