202d6fac4afdc544a3cb2724cc8a7236354b5d34
Gabryelle SS <gabryelle-gaby@hotmail.com>
2020-05-02 18:57:38 -0300

    feat: applying validation required fields
    Resolves: PROJ-28

--- src/br/com/senac/school/controller/UserRegisterController.java
+++ src/br/com/senac/school/controller/UserRegisterController.java
@@ -1,49 +1,192 @@
 package br.com.senac.school.controller;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RequiredFieldValidator;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-public class UserRegisterController {
+public class UserRegisterController implements Initializable {
 	
 	@FXML
-    private TextField fieldFirstName;
+    private JFXTextField fieldFirstName;
     @FXML
-    private TextField fieldFirstLastName;
+    private JFXTextField fieldLastName;
     @FXML
-    private TextField fieldCPF;
+    private JFXTextField fieldEmail;
     @FXML
-    private DatePicker fieldDateBirth;
+    private JFXPasswordField txt;
     @FXML
-    private TextField fieldMaritalStatus;
+    private JFXPasswordField fieldPassword;
     @FXML
-    private TextField fieldCell;
+    private JFXPasswordField fieldConfirmPassword;
     @FXML
-    private TextField fieldTephone;
+    private JFXTextField fieldCPF;
     @FXML
-    private Pane modalNotificationWarning;
+    private JFXDatePicker fieldDateBirth;
     @FXML
-    private Label textNotification;
+    private JFXComboBox fieldMaritalStatus;
+    @FXML
+    private JFXTextField fieldTephone;
+    @FXML
+    private JFXTextField fieldCell;
     @FXML
     private HBox formFieldsRegisterUser;
     @FXML
     private VBox formFieldsRegisterUserAddress;
+    @FXML
+    private Button btnNextField;
+    @FXML
+    private Pane modalWarning;
+    @FXML
+    private Text labelWarning;
+    
+	String warningModal = "Ops! Você precisa preencher os campos obrigatórios.";
+	String warningModalPassawordIncorrect = "Ops! As senhas estão diferentes!";
+    
+    @Override
+	public void initialize(URL location, ResourceBundle resources) {
+    	fieldRequired();
+    	
+    	modalWarning.setVisible(false);
+    	formFieldsRegisterUserAddress.setVisible(false);
+	}
+    
+    void fieldRequired() {
+    	RequiredFieldValidator validator = new RequiredFieldValidator();
+    	validator.setMessage("Campo obrigatório!");
+    	
+    	fieldFirstName.getValidators().add(validator);
+    	fieldFirstName.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) {
+            	fieldFirstName.validate();
+            }
+        });
+    	
+    	fieldLastName.getValidators().add(validator);
+    	fieldLastName.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) {
+            	fieldLastName.validate();
+            }
+        });
+    	
+    	fieldEmail.getValidators().add(validator);
+    	fieldEmail.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) {
+            	fieldEmail.validate();
+            }
+        });
+    	
+    	fieldPassword.getValidators().add(validator);
+    	fieldPassword.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) {
+            	fieldPassword.validate();
+            }
+        });
+    	
+    	fieldConfirmPassword.getValidators().add(validator);
+    	fieldConfirmPassword.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) {
+            	fieldConfirmPassword.validate();
+            }
+        });
+    	
+    	fieldCell.getValidators().add(validator);
+    	fieldCell.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) {
+            	fieldCell.validate();
+            }
+        });
+    }
+    
+    private boolean checkRequiredFields() {    	
+    	boolean firstNameEmpty = fieldFirstName.getText().trim().isEmpty();
+    	boolean lastNameEmpty = fieldLastName.getText().trim().isEmpty();
+    	boolean email = fieldEmail.getText().trim().isEmpty();
+    	boolean password = fieldPassword.getText().trim().isEmpty();
+		boolean passwordConfirm = fieldConfirmPassword.getText().trim().isEmpty();
+		boolean cell = fieldCell.getText().trim().isEmpty();
+		
+		ArrayList<Boolean> fieldsRequireds = new ArrayList();
+		
+		fieldsRequireds.add(firstNameEmpty);
+		fieldsRequireds.add(lastNameEmpty);
+		fieldsRequireds.add(email);
+		fieldsRequireds.add(password);
+		fieldsRequireds.add(passwordConfirm);
+		fieldsRequireds.add(cell);
+		
+		System.out.println(firstNameEmpty);
+		
+		if (confirmationSamePasswords()) {
+			labelWarning.setText(warningModalPassawordIncorrect);
+    		modalWarning.setVisible(true);
+    		
+    		return true;
+		}
+		else {
+			for (Boolean fieldRequired : fieldsRequireds) {
+				if (fieldRequired == true) {
+					labelWarning.setText(warningModal);
+		    		modalWarning.setVisible(true);
+		    		return true;
+				}
+			}
+			return false;
+		}		
+    }
+    
+    private boolean confirmationSamePasswords() {
+    	String password = fieldPassword.getText();
+    	String confirmPassaword = fieldConfirmPassword.getText();
+    	
+    	if (password.equals(confirmPassaword)) {
+    		return false;
+    	} else {
+    		return true;
+    	}
+    }
     
     @FXML
     private void handleNextFields(ActionEvent event) {
-    	formFieldsRegisterUser.setStyle("-fx-opacity: 0;");
-    	formFieldsRegisterUserAddress.setStyle("-fx-opacity: 1;");
+    	
+    	if (checkRequiredFields()) {
+    		System.out.println("Senha incorreta");
+    	} else {
+        	formFieldsRegisterUser.setVisible(false);
+        	formFieldsRegisterUserAddress.setVisible(true);
+        	
+        	btnNextField.setText("Cadastrar");
+    	}
     }
     
     @FXML
     private void handleBackRegisterUser(ActionEvent event) {
-    	formFieldsRegisterUser.setStyle("-fx-opacity: 1;");
-    	formFieldsRegisterUserAddress.setStyle("-fx-opacity: 0;");
+    	formFieldsRegisterUser.setVisible(true);
+    	formFieldsRegisterUserAddress.setVisible(false);
+    	
+    	String btnText = btnNextField.getText();
+    	if (btnText == "Cadastrar") {
+        	btnNextField.setText("Próximo");
+    	}
     }
-	
+    
+    @FXML
+    private void btnConfirm(ActionEvent event) { 
+    	modalWarning.setVisible(false);
+    }
+    	
 }
